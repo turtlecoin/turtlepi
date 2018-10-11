a4ab892f20afa97d5b61b026f8d9a539bb91add5
crappyrules <crappyrules@protonmail.com>
2018-10-11 01:07:03 -0400

    update README & script update

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 
 ![image](https://github.com/crappyrules/turtlepi/blob/master/img/TURTLEPI.png)
 Everything turtley in one place
+
+
+### Setup
+
+- Right click [this link](https://github.com/turtlecoin/turtlepi/raw/master/scripts/turtlepi.sh) and choose `Save link as...` to download the latest easy setup script.
+- Or from the shell enter `wget https://github.com/turtlecoin/turtlepi/raw/master/scripts/turtlepi.sh` then `chmod +x turtlepi.sh` and finally `./turtlepi.sh`.
+- Sit back and relax for 15 minutes.
--- scripts/turtlepi.sh
+++ scripts/turtlepi.sh
@@ -14,7 +14,15 @@ sudo apt install -y build-essential python-dev gcc g++ git cmake libboost-all-de
 # Installation of Nodejs and NPM
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 sudo apt-get install -y nodejs
+#install php 7.2 for the web wallet
+wget -q https://packages.sury.org/php/apt.gpg -O- | sudo apt-key add -
+echo "deb https://packages.sury.org/php/ stretch main" | sudo tee /etc/apt/sources.list.d/php.list
+sudo apt-get install -y ca-certificates apt-transport-https
+sudo apt update
+sudo apt-get install -y php-fpm php-mcrypt php-cli php-mysql php-gd php-imagick php-recode php-tidy php-xmlrpc
 # Grab the latest release of TurtleCoin from github and extract it
 wget https://github.com/turtlecoin/turtlecoin/releases/download/v0.8.3/turtlecoin-v0.8.3-aarch64.tar.gz
 tar xvf turtlecoin-v0.8.3-aarch64.tar.gz
+cd turtlecoin-v0.8.3
+mv zedwallet turtle-service /home/pi/
 echo "TurtleCoin is now installed and ready!"
