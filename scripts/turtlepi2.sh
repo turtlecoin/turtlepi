1981d7cb3b336b6f2aac239f950d3c4879c58d13
crappyrules <crappyrules@protonmail.com>
2018-10-15 17:01:43 -0400

    final first draft?

--- scripts/turtlepi2.sh
+++ scripts/turtlepi2.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-screen -d -m -S turtlewallet bash -c './turtle-service -w mywallet -p changeme --rpc-password test --bind-port 8070 --bind-address 0.0.0.0 --daemon-address shitfunnel.shitcoin.works --daemon-port 11898'
+screen -d -m -S turtlewallet bash -c './turtle-service -w mywallet -p changeme --rpc-password test --bind-port 8070 --bind-address 0.0.0.0 --daemon-address turtlenode.turtlepi.org --daemon-port 11898'
